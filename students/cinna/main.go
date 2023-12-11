61d29b877b774c85b02c78abad0fa09ede152bbb
CinnamonrollSaigo <121988836+CinnamonrollSaigo@users.noreply.github.com>
2023-12-11 09:21:17 +0000

    ex1-part1

---
+++ students/cinna/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+func main(){
+	//open the csv file
+	file,err := os.Open("/workspaces/quiz/problems.csv")
+	if err!=nil {
+		fmt.Println("error when opening csv file",err)
+		return
+	}
+	defer file.Close()
+
+	//create a reader
+	reader := csv.NewReader(file)
+
+	//read data lines
+	records, err:= reader.ReadAll()
+	if err!=nil {
+		fmt.Println("error when reading data",err)
+		return
+	}
+
+	//process data
+	for _,record :=  range records{
+		for _,value := range record {
+			fmt.Printf("%s",value)
+		}
+		fmt.Println()
+	}
+
+
+
+
+}
