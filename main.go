4af9ae60b788836623cd9d29a8448c8a0c422344
CinnamonrollSaigo <121988836+CinnamonrollSaigo@users.noreply.github.com>
2023-12-13 15:19:33 +0000

    just complete part1

--- main.go
+++ main.go
@@ -5,52 +5,56 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"quiz/students/hackeryarn/problem"
+	"strings"
 )
 
+
 func main() {
-	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'questions,answer'")
+	csvFilename := flag.String("csv","problems.csv","a csv file in the format of 'questions,answer'")
 	flag.Parse()
 
-	file, err := os.Open(*csvFilename)
-	if err != nil {
-		exit(fmt.Sprintf("failed to open csv file: %s", *csvFilename))
+	file,err := os.Open(*csvFilename)
+	if err!=nil {
+		exit(fmt.Sprintf("failed to open csv file: %s",*csvFilename))
 	}
 	r := csv.NewReader(file)
-	lines, err := r.ReadAll()
+	lines,err:=r.ReadAll()
 	if err != nil {
 		exit("failed to parse the csv file")
 	}
 	problems := parseLines(lines)
 
 	correct := 0
-	for i, p := range problems {
-		fmt.Printf("problem #%d:%s=\n", i+1, p.q)
+	for i,p :=range problems {
+		fmt.Printf("problem #%d:%s= \n",i+1,p.q)
 		var answer string
-		fmt.Scanf("%s\n", &answer)
+		fmt.Scanf("%s\n",&answer)
 		if answer == p.a {
-			correct++
+			correct++		
 		}
 	}
-	fmt.Println("you scored %d out of %d.\n!", correct, len(problems))
+    fmt.Printf("you scored %d out of %d.\n",correct,len(problems))
 }
 
-func parseLines(lines [][]string) []problem {
-	ret := make([]problem, len(lines))
-	for i, line := range lines {
-		ret[i] = problem{
-			q: line[0],
-			a: line[1],
-		}
+type problem struct{
+	q string
+	a string
+}
 
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem,len(lines))
+	for i,line := range lines{
+		ret[i]=problem{
+			q:line[0],
+			a:strings.TrimSpace(line[1]),
+		}	
 	}
 	return ret
 }
 
-type problem struct {
-	q string
-	a string
-}
+
+
 
 func exit(msg string) {
 	fmt.Println(msg)
